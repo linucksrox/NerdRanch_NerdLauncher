147e47b7a4c4405dafc8ae3ae8ec1b21019272ed
linucksrox <ericdalyblog@dalydays.com>
2018-07-18 07:05:08 -0400

    Sort list of launcher activities, page 1596

--- app/src/main/java/com/dalydays/android/nerdlauncher/NerdLauncherFragment.kt
+++ app/src/main/java/com/dalydays/android/nerdlauncher/NerdLauncherFragment.kt
@@ -1,18 +1,24 @@
 package com.dalydays.android.nerdlauncher
 
+import android.content.Intent
+import android.content.pm.ResolveInfo
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_nerd_launcher.*
+import java.util.*
 
 class NerdLauncherFragment : Fragment() {
     private var mRecyclerView: RecyclerView? = null
 
     companion object {
+        private val TAG = "NerdLauncherFragment"
+
         fun newInstance(): NerdLauncherFragment {
             return NerdLauncherFragment()
         }
@@ -23,6 +29,27 @@ class NerdLauncherFragment : Fragment() {
         mRecyclerView = v.findViewById(R.id.app_recycler_view)
         mRecyclerView!!.layoutManager = LinearLayoutManager(activity)
 
+        setupAdapter()
         return v
+    }
+
+    private fun setupAdapter() {
+        val startupIntent = Intent(Intent.ACTION_MAIN)
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER)
+        val packageManager = activity!!.packageManager
+        val activities = packageManager.queryIntentActivities(startupIntent, 0)
+
+        Collections.sort(activities, object: Comparator < ResolveInfo > {
+            override fun compare(a: ResolveInfo?, b: ResolveInfo?): Int {
+                val packageManager = activity!!.packageManager
+                return String.CASE_INSENSITIVE_ORDER.compare(
+                        a!!.loadLabel(packageManager).toString(),
+                        b!!.loadLabel(packageManager).toString())
+            }
+        })
+
+        }
+
+        Log.i(TAG, "Found " + activities.size + " activities.")
     }
 }
--- app/src/main/java/com/dalydays/android/nerdlauncher/SingleFragmentActivity.kt
+++ app/src/main/java/com/dalydays/android/nerdlauncher/SingleFragmentActivity.kt
@@ -3,7 +3,6 @@ package com.dalydays.android.nerdlauncher
 import android.os.Bundle
 import android.support.annotation.LayoutRes
 import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
 
 abstract class SingleFragmentActivity : AppCompatActivity() {
